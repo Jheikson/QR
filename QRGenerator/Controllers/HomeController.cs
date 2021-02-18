dd79912cc12578b42bbc7095f086ac807a3be91c
Jheikson Vaamonde <javier_16_00@hotmail.com>
2021-02-18 11:14:34 -0400

    Actualizacion del repo

--- QRGenerator/Controllers/HomeController.cs
+++ QRGenerator/Controllers/HomeController.cs
@@ -1,16 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 using System.Web.Mvc;
+using System.Drawing;
 
 namespace QRGenerator.Controllers
 {
     public class HomeController : Controller
-    {
+    {        
         public ActionResult Index()
         {
+            GeneratePDF("C:\\Users\\Jheikson\\Desktop", "PDF_QR", "https://localhost:44371/siniestro/83943424");
             return View();
+        }
+
+        private static byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+
+        private static void GeneratePDF(string ruta, string nombreArchivo, string qrText)
+        {
+            Document doc = new Document(PageSize.A4);
+            var output = new FileStream($"{ruta}\\{nombreArchivo}.pdf", FileMode.Create);
+            var writer = PdfWriter.GetInstance(doc, output);
+
+            doc.Open();
+            BarcodeQRCode qrCode = new BarcodeQRCode(qrText, 100, 100, null);
+            var image = qrCode.GetImage();
+            image.ScaleToFitHeight = false;
+            //image.SetAbsolutePosition(400f, 700f);
+            image.SetDpi(600, 600);
+            doc.Add(image);
+
+            doc.Close();
         }
 
         public ActionResult About()
